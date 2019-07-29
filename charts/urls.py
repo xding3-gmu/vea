6ee0f3aa7ed8a1f0ff4ea576da9d289d9ce37e68
xding3-gmu <xding3@gmu.edu>
2019-07-29 14:39:58 -0400

    urls change

--- charts/urls.py
+++ charts/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),
-    path('api/', views.get_data, name='api-data'),
+    path('vea/', views.HomeView.as_view(), name='home'),
+    path('vea/api/', views.get_data, name='api-data'),
 ]
